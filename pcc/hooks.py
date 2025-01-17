1adaff25918275937d06915b83547341231fa892
sivajyothis7 <sivajyothis9446@gmail.com>
2025-01-17 15:58:02 +0530

    first commit

--- pcc/hooks.py
+++ pcc/hooks.py
@@ -227,3 +227,7 @@ app_license = "mit"
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
 
+# Custom fields for Sales Invoice
+fixtures = [
+    {"dt": "Custom Field", "filters":[["module", "=", "pcc"]]}
+]
---
+++ pcc/pcc/doctype/job_record/job_record.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, siva@enfono.in and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Job Record", {
+// 	refresh(frm) {
+
+// 	},
+// });
