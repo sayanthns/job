68b8acf9fbc3a650b0bbf8194ea2f0c059f8ad55
sivajyothis7 <sivajyothis9446@gmail.com>
2025-01-28 14:53:26 +0530

    new

--- pcc/pcc/patches/override_payment_entry.py
+++ pcc/pcc/patches/override_payment_entry.py
@@ -6,8 +6,8 @@ from erpnext.accounts.doctype.payment_entry.payment_entry import PaymentEntry
 
 class CustomPE(PaymentEntry):
     def set_amounts(self):
-		self.set_received_amount()
-		self.set_amounts_in_company_currency()
-		self.set_total_allocated_amount()
-		self.set_unallocated_amount()
-		self.set_difference_amount()
+        self.set_received_amount()
+        self.set_amounts_in_company_currency()
+        self.set_total_allocated_amount()
+        self.set_unallocated_amount()
+        self.set_difference_amount()
