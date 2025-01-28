5e7f0253d544d9065ac8d838869af4d811059d62
sivajyothis7 <sivajyothis9446@gmail.com>
2025-01-28 15:10:20 +0530

    new

--- pcc/pcc/patches/override_payment_entry.py
+++ pcc/pcc/patches/override_payment_entry.py
@@ -1,13 +1,10 @@
 import frappe
-import json
 from frappe import _
 from frappe.model.document import Document
 from erpnext.accounts.doctype.payment_entry.payment_entry import PaymentEntry
 
 class CustomPE(PaymentEntry):
-    def set_amounts(self):
-        self.set_received_amount()
-        self.set_amounts_in_company_currency()
-        self.set_total_allocated_amount()
-        self.set_unallocated_amount()
-        self.set_difference_amount()
+    
+    def set_exchange_gain_loss(self):
+        # Simply return without performing any exchange gain/loss calculation
+        return
